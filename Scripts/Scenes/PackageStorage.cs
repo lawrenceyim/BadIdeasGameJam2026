125a2ee912e25df71df480f51ce78528d5377e53
lawrenceyim <lawrence.yim.dev@gmail.com>
2026-03-04 12:37:40 -0500

    Fixed issue with dragging, but edge case for alt-tab still remains.

--- Scripts/Scenes/PackageStorage.cs
+++ Scripts/Scenes/PackageStorage.cs
@@ -6,106 +6,120 @@ using InputSystem;
 using ServiceSystem;
 
 public partial class PackageStorage : Node2D, IInputState {
-    [Export]
-    private PackedScene _placeholderTile = null!;
+	[Export]
+	private PackedScene _placeholderTile = null!;
 
-    [Export]
-    private PackedScene _placeholderPackage = null!;
+	[Export]
+	private PackedScene _placeholderPackage = null!;
 
-    private readonly Dictionary<StorageTile, Vector2I> _tilePositions = [];
+	private readonly Dictionary<StorageTile, Vector2I> _tilePositions = [];
 
-    private int _rows = 5;
-    private int _columns = 5;
-    private int _xOffset = 32;
-    private int _yOffset = 32;
+	private int _rows = 5;
+	private int _columns = 5;
+	private int _xOffset = 32;
+	private int _yOffset = 32;
 
-    private StorageTile?[,] _tiles = null!;
-    private StorageTile? _currentTile;
-    private PackageGO? _currentPackage;
-    private bool _dragPackage = false;
-    
-    public override void _Ready() {
-        ServiceLocator serviceLocator = GetNode<ServiceLocator>(ServiceLocator.AutoloadPath);
-        InputStateMachine inputStateMachine = serviceLocator.GetService<InputStateMachine>();
-        inputStateMachine.SetState(this);
+	private StorageTile?[,] _tiles = null!;
+	private StorageTile? _currentTile;
+	private PackageGO? _currentPackage;
+	private Vector2I? _lastPackageTile;
+	private PackageGO? _lastPackage;
+	private bool _dragPackage = false;
 
-        _tiles = new StorageTile[_rows, _columns];
-        _CreateGrid();
-        _CreatePlaceholderPackage();
-    }
+	public override void _Ready() {
+		ServiceLocator serviceLocator = GetNode<ServiceLocator>(ServiceLocator.AutoloadPath);
+		InputStateMachine inputStateMachine = serviceLocator.GetService<InputStateMachine>();
+		inputStateMachine.SetState(this);
 
-    public void ProcessInput(InputEventDto eventDto) {
-        if (eventDto is MouseButtonPressedDto && _currentPackage != null) {
-            _currentPackage.SetOpacity(PackageGO.Opacity.Half);
-            _dragPackage = true;
-        }
+		_tiles = new StorageTile[_rows, _columns];
+		_CreateGrid();
+		_CreatePlaceholderPackage();
+	}
 
-        if (eventDto is MouseButtonReleasedDto) {
-            if (_dragPackage) {
-                _currentPackage?.SetOpacity(PackageGO.Opacity.Full);
-                _dragPackage = false;
-            }
-        }
+	public void ProcessInput(InputEventDto eventDto) {
+		if (eventDto is MouseButtonPressedDto && _currentPackage != null) {
+			GD.Print("Pressed");
+			_currentPackage.SetOpacity(PackageGO.Opacity.Half);
+			_dragPackage = true;
+		}
 
-        if (eventDto is MouseMotionDto mouseMotion) {
-            if (!_dragPackage) {
-                return;
-            }
+		if (eventDto is MouseButtonReleasedDto) {
+			GD.Print("Released");
+			if (_dragPackage) {
+				_currentPackage?.SetOpacity(PackageGO.Opacity.Full);
+				_dragPackage = false;
+			}
+		}
 
-            GD.Print(mouseMotion.Position);
-            _currentPackage!.Position += mouseMotion.Relative;
-        }
-    }
+		if (eventDto is MouseMotionDto mouseMotion) {
+			if (!_dragPackage) {
+				return;
+			}
 
-    private void _CreatePlaceholderPackage() {
-        PackageGO package = (PackageGO)_placeholderPackage.Instantiate();
-        package.Position = new Vector2(300, 0);
-        AddChild(package);
-        package.Hovered += _HandlePackageHover;
-    }
+			if (_currentPackage is null) {
+				_dragPackage = false;
+				return;
+			}
 
-    private void _CreateGrid() {
-        for (int row = 0; row < _rows; row++) {
-            for (int column = 0; column < _columns; column++) {
-                StorageTile tile = (_placeholderTile.Instantiate() as StorageTile)!;
-                _tilePositions[tile] = new Vector2I(row, column);
-                _tiles[row, column] = tile;
-                AddChild(tile);
-                tile.Position = new Vector2(column * _xOffset, row * _yOffset);
-                tile.Hovered += _HandleStorageTileHover;
-            }
-        }
-    }
+			_currentPackage.Position += mouseMotion.Relative;
+		}
+	}
 
-    private void _ResetHighlights() {
-        foreach (StorageTile tile in _tilePositions.Keys) {
-            tile.SetHighlight(StorageTile.Highlight.None);
-        }
-    }
+	private void _CreatePlaceholderPackage() {
+		PackageGO package = (PackageGO)_placeholderPackage.Instantiate();
+		package.Position = new Vector2(300, 0);
+		AddChild(package);
+		package.Hovered += _HandlePackageHover;
+	}
 
-    private void _HandlePackageHover(PackageGO package, bool hovered, Vector2I tilePosition) {
-        GD.Print($"Hovered {hovered} {tilePosition}");
-        // Safety measure for cases where someone may alt-tab or something so package is no longer hovered
-        _currentPackage?.SetOpacity(PackageGO.Opacity.Full);
-        _dragPackage = false;
-        
-        // hovered false is called after hover entered when mouse cursor enters new tile?
-        _currentPackage = null;
-        if (hovered) {
-            _currentPackage = package;
-        }
-    }
+	private void _CreateGrid() {
+		for (int row = 0; row < _rows; row++) {
+			for (int column = 0; column < _columns; column++) {
+				StorageTile tile = (_placeholderTile.Instantiate() as StorageTile)!;
+				_tilePositions[tile] = new Vector2I(row, column);
+				_tiles[row, column] = tile;
+				AddChild(tile);
+				tile.Position = new Vector2(column * _xOffset, row * _yOffset);
+				tile.Hovered += _HandleStorageTileHover;
+			}
+		}
+	}
 
-    private void _HandleStorageTileHover(StorageTile tile, bool hovered) {
-        if (hovered) {
-            _currentTile = tile;
-            GD.Print($"New hovered tile. {_tilePositions[tile]}");
-            return;
-        }
+	private void _ResetHighlights() {
+		foreach (StorageTile tile in _tilePositions.Keys) {
+			tile.SetHighlight(StorageTile.Highlight.None);
+		}
+	}
 
-        if (_currentTile == tile) {
-            GD.Print("No hovered tile");
-            _currentTile = null;
-        }
-    }
+	private void _HandlePackageHover(PackageGO package, bool hovered, Vector2I tilePosition) {
+		// // GD.Print($"Hovered {hovered} {tilePosition}");
+		// // Safety measure for cases where someone may alt-tab or something so package is no longer hovered
+		// _currentPackage?.SetOpacity(PackageGO.Opacity.Full);
+		// _dragPackage = false;
+
+		// hovered false is called after hover entered when mouse cursor enters new tile?
+		if (hovered) {
+			_currentPackage = package;
+			_lastPackageTile = tilePosition;
+			_lastPackage = package;
+			return;
+		}
+
+		if (_lastPackage == package && tilePosition == _lastPackageTile) {
+			_currentPackage = null;
+		}
+	}
+
+	private void _HandleStorageTileHover(StorageTile tile, bool hovered) {
+		if (hovered) {
+			_currentTile = tile;
+			// GD.Print($"New hovered tile. {_tilePositions[tile]}");
+			return;
+		}
+
+		if (_currentTile == tile) {
+			// GD.Print("No hovered tile");
+			_currentTile = null;
+		}
+	}
 }
