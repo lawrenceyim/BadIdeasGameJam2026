3a6105bc0deb30b089c62a7d421072d5040eba5d
lawrenceyim <lawrence.yim.dev@gmail.com>
2026-03-05 10:26:26 -0500

    Formatted code.

--- Scripts/Repository/Impl/TextureRepository.cs
+++ Scripts/Repository/Impl/TextureRepository.cs
@@ -3,17 +3,17 @@ using Godot.Collections;
 using RepositorySystem;
 
 public partial class TextureRepository : Node, IAutoload, IRepository {
-	public static string AutoloadPath => "/root/TextureRepository";
+    public static string AutoloadPath => "/root/TextureRepository";
 
-	[Export]
-	private Dictionary<TextureId, Texture2D> _textures;
+    [Export]
+    private Dictionary<TextureId, Texture2D> _textures;
 
-	public Texture2D GetTexture(TextureId id) {
-		return _textures[id];
-	}
+    public Texture2D GetTexture(TextureId id) {
+        return _textures[id];
+    }
 }
 
 public enum TextureId {
-	// Ingredients
-	PlaceHolder = 0,
+    // Ingredients
+    PlaceHolder = 0,
 }
